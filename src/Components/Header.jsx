9931d040185123d2455edd74376d373299e3d65b
Oluwanifemi <temenuoluwanifemi2005@gmail.com>
2023-09-18 17:01:40 +0100

    Made a hero section above the movies section

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header sm:h-[100vh] bg-no-repeat bg-center'>
+        <p className='text-white'>Header</p>
+    </div>
+  )
+}
+
+export default Header
