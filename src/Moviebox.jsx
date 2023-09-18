89da05c86d6ae8cf3fb8db2ea05263ac3a22d94c
Oluwanifemi <temenuoluwanifemi2005@gmail.com>
2023-09-18 17:01:40 +0100

    Made a hero section above the movies section

--- src/Moviebox.jsx
+++ src/Moviebox.jsx
@@ -4,10 +4,11 @@ const API_IMG="https://image.tmdb.org/t/p/w500/";
 
 function Moviebox({original_title, poster_path, vote_average, release_date, overview}) {
   return (
-    <div>
+    <div className=''>
+        <img src={API_IMG+poster_path} className='' alt="" />
         <h3>{original_title}</h3>
-        <img src={API_IMG+poster_path} alt="" />
-        <p> {overview} </p>
+        <button>View more</button>
+        {/* <p>{overview}</p> */}
     </div>
   )
 }
